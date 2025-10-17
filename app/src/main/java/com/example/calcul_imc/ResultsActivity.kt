020d0f6685ec3c333fd06ebda83ca705f5ecd1ab
ldloler <marc.be.pi@gmail.com>
2025-10-17 19:00:07 +0200

    En creació dels resoltats del calcul de imc

---
+++ app/src/main/java/com/example/calcul_imc/ResultsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.calcul_imc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+    }
+}
