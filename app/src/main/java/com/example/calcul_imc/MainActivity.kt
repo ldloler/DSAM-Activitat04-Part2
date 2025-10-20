c57755bd4cf43ffa823682932d93ffdd3f531e17
ldloler <marc.be.pi@gmail.com>
2025-10-20 18:55:57 +0200

    Final de classe

--- app/src/main/java/com/example/calcul_imc/MainActivity.kt
+++ app/src/main/java/com/example/calcul_imc/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.calcul_imc
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val maleButton = findViewById<ImageView>(R.id.ic_male)
+        val femaleButton = findViewById<ImageView>(R.id.ic_female)
+        val slider = findViewById<Slider>(R.id.slider)
+        val textSlider = findViewById<TextView>(R.id.numSlider)
+        val numWeight = findViewById<TextView>(R.id.numWeight)
+        val weightPlus = findViewById<ImageView>(R.id.weightPlus)
+        val weightMinus = findViewById<ImageView>(R.id.weightMinus)
+        val numAge = findViewById<TextView>(R.id.numAge)
+        val agePlus = findViewById<ImageView>(R.id.agePlus)
+        val ageMinus = findViewById<ImageView>(R.id.ageMinus)
+
+        slider.addOnChangeListener { slider, value, fromUser -> textSlider.text = value.toString() }
     }
 }
