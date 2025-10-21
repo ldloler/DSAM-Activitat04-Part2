eabc5a18f3bb7295ddb1f80e229fcf5d78dd8709
ldloler <marc.be.pi@gmail.com>
2025-10-21 19:20:40 +0200

    Finalitzat

--- app/src/main/java/com/example/calcul_imc/MainActivity.kt
+++ app/src/main/java/com/example/calcul_imc/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.calcul_imc
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.slider.Slider
+import kotlin.math.round
+import kotlin.math.sqrt
+import kotlin.math.truncate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,45 @@ class MainActivity : AppCompatActivity() {
         val numAge = findViewById<TextView>(R.id.numAge)
         val agePlus = findViewById<ImageView>(R.id.agePlus)
         val ageMinus = findViewById<ImageView>(R.id.ageMinus)
+        val calculateButton = findViewById<LinearLayout>(R.id.btnCalculate)
 
         slider.addOnChangeListener { slider, value, fromUser -> textSlider.text = value.toString() }
+
+        weightPlus.setOnClickListener {
+            if (numWeight.text.toString().toInt() >= 25 && numWeight.text.toString().toInt() < 200) {
+                var num = numWeight.text.toString().toInt()
+                numWeight.text = (num + 1).toString()
+            }
+        }
+
+        weightMinus.setOnClickListener {
+            if (numWeight.text.toString().toInt() > 25 && numWeight.text.toString().toInt() <= 200) {
+                var num = numWeight.text.toString().toInt()
+                numWeight.text = (num - 1).toString()
+            }
+        }
+
+        agePlus.setOnClickListener {
+            if (numAge.text.toString().toInt() >= 14 && numAge.text.toString().toInt() < 100) {
+                var num = numAge.text.toString().toInt()
+                numAge.text = (num + 1).toString()
+            }
+        }
+
+        ageMinus.setOnClickListener {
+            if (numAge.text.toString().toInt() > 14 && numAge.text.toString().toInt() <= 100) {
+                var num = numAge.text.toString().toInt()
+                numAge.text = (num - 1).toString()
+            }
+        }
+
+        calculateButton.setOnClickListener {
+            val peso = numWeight.text.toString().toDouble()
+            val altura = textSlider.text.toString().toDouble() / 100
+            val numIMC = round((peso / (altura * altura)) * 100) / 100
+            val intent = Intent(this, ResultsActivity::class.java)
+            intent.putExtra("numIMC", numIMC.toString())
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/calcul_imc/ResultsActivity.kt
+++ app/src/main/java/com/example/calcul_imc/ResultsActivity.kt
@@ -1,6 +1,12 @@
 package com.example.calcul_imc
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +16,40 @@ class ResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        val textCalculIMC = findViewById<TextView>(R.id.textCalculIMC)
+        val numCalculIMC = findViewById<TextView>(R.id.numCalculIMC)
+        val botoEnrere = findViewById<ImageView>(R.id.goBack)
+        val recalculate = findViewById<LinearLayout>(R.id.btReCalculate)
+
+        val numIMC = intent.getStringExtra("numIMC").toString().toDouble()
+
+        numCalculIMC.text = numIMC.toString()
+
+        val tipoPes = when {
+            numIMC < 18.5 -> "Insufficient weight"
+            numIMC in 18.5..24.9 -> "Normal"
+            numIMC in 25.0..29.9 -> "Overweight"
+            numIMC >= 30 -> "Obesity"
+            else -> "Error"
+        }
+
+        textCalculIMC.text = tipoPes
+
+        when (tipoPes) {
+            "Insufficient weight" -> textCalculIMC.setTextColor(Color.RED)
+            "Normal" -> textCalculIMC.setTextColor(Color.GREEN)
+            "Overweight" -> textCalculIMC.setTextColor(Color.YELLOW)
+            "Obesity" -> textCalculIMC.setTextColor(Color.RED)
+        }
+
+        botoEnrere.setOnClickListener {
+            finish()
+        }
+
+        recalculate.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
